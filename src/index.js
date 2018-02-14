3573d31f1663706e2d5429e81ac9927cf3a42bff
Dungeon_Master <sahil@excellencetechnologies.in>
2018-02-14 16:50:47 +0530

    Screen 1 Login Page

---
+++ src/index.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from './index.css';
+import { Form,Col,ItemLayout, Icon, Input, Button, Checkbox } from 'antd';
+import style from 'antd/dist/antd.css';
+
+const FormItem = Form.Item;
+
+class NormalLoginForm extends React.Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+    <div id="container">
+      <Col id="col" span={10} offset={7}>
+      <div id="content">
+      <h1>Login</h1>
+
+      <Form onSubmit={this.handleSubmit} className="login-form">
+      <FormItem >
+      E-Mail Address*
+        {getFieldDecorator('email', {
+          rules: [{
+            type: 'email', message: 'The input is not valid E-mail!',
+          }, {
+            required: true, message: 'Please input your E-mail!',
+          }],
+        })(
+          <Input type="text" />
+        )}
+      </FormItem>
+        <FormItem>
+        Password*
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please input your Password!' }],
+          })(
+            <Input  type="password" />
+          )}
+        </FormItem>
+        <FormItem>
+        <Button htmlType="submit" id="login-form-button" className="login-form-button">
+          Login
+        </Button>
+          <div id="links">
+          <a  href="#">Forgot password? Reset it!</a><br />
+
+          <a  href="#">No Account yet? Register now!</a>
+          </div>
+        </FormItem>
+      </Form>
+      </div>
+      </Col>
+      </div>
+    );
+  }
+}
+
+const WrappedNormalLoginForm = Form.create()(NormalLoginForm);
+
+ReactDOM.render(<WrappedNormalLoginForm />, document.getElementById('root'));
