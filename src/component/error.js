4ebe0af37d85c13204ff19b2de2c54fc650ef4d3
Dungeon_Master <sahil@excellencetechnologies.in>
2018-02-15 10:31:22 +0530

    Added an error screen to prevent url injection

---
+++ src/component/error.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Icon} from 'antd';
+
+const error =()=> (
+  <div>
+<Icon id="icon" type="frown-o" /><h1 style={{color:'red'}}>Error 404 Not Found!</h1>
+</div>
+);
+
+export default error;
