9a140d7c6d90d4ec3dbfd6ef24b54e77db99c3bf
Dungeon_Master <sahil@excellencetechnologies.in>
2018-02-15 10:57:33 +0530

    Added an error screen to prevent url injection

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import {Switch,BrowserRouter,Route,Link} from 'react-router-dom';
 import WrappedNormalLoginForm from './component/Login.js';
 import error from './component/error.js';
 
-
 const routes=(
     <BrowserRouter>
-    <Switch>
-        <Route path="/" component={WrappedNormalLoginForm} exact/>
-        <Route component={error}/>
-    </Switch>
+      <Switch>
+          <Route path="/" component={WrappedNormalLoginForm} exact/>
+          <Route component={error}/>
+          </Switch>
     </BrowserRouter>
 );
 
--- src/component/error.js
+++ src/component/error.js
@@ -1,12 +1,20 @@
-
-import React from 'react';
 import ReactDOM from 'react-dom';
-import {Icon} from 'antd';
+import React from 'react';
+import styles from '../index.css';
+import { Form,Col,ItemLayout, Icon, Input, Button, Checkbox } from 'antd';
+import style from 'antd/dist/antd.css';
+import {BrowserRouter,Route,Link} from 'react-router-dom';
 
 const error =()=> (
-  <div>
-<Icon id="icon" type="frown-o" /><h1 style={{color:'red'}}>Error 404 Not Found!</h1>
-</div>
+  <div id="container">
+    <Col id="col" span={10} offset={7}>
+      <div id="content">
+        <div id="errorMessage">
+          <Icon id="icon" type="frown-o" /><h1 style={{color:'red'}}>Error 404 Not Found!</h1>
+        </div>
+      </div>
+    </Col>
+  </div>
 );
 
 export default error;
