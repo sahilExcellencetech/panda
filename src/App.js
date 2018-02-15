cac7eb6f1c0b70990db80cbcd31206f198ae8ed2
saurabh singh <sau9458@gmail.com>
2018-02-15 12:03:18 +0530

    screen2 forget password

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Switch,BrowserRouter,Route,Link} from 'react-router-dom';
 import WrappedNormalLoginForm from './component/Login.js';
+import WrappedForgetPasswordForm from'./component/ForgetPassword.js';
 
 const routes=(
     <BrowserRouter>
     <Switch>
         <Route path="/" component={WrappedNormalLoginForm} exact/>
+        <Route path="/forgetpassword"component={WrappedForgetPasswordForm}/>
     </Switch>
     </BrowserRouter>
 );
---
+++ src/component/ForgetPassword.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import style from 'antd/dist/antd.css';
+import styles from '../index.css';
+import { Col,Form, Icon, Input, Button} from 'antd';
+import {BrowserRouter,Route,Link} from 'react-router-dom';
+const FormItem = Form.Item;
+
+class ForgetPassword extends React.Component {
+ handleSubmit = (e) => {
+   e.preventDefault();
+   this.props.form.validateFields((err, values) => {
+     if (!err) {
+       console.log('Received values of form: ', values);
+     }
+   });
+ }
+ render() {
+   const { getFieldDecorator } = this.props.form;
+   return (
+     <Col id="col" span={10} offset={7}>
+     <div id="content">
+     <Form onSubmit={this.handleSubmit} className="login-form">
+
+     <FormItem >
+ E-mail Address*
+    {getFieldDecorator('email', {
+      rules: [{
+        type: 'email', message: 'The input is not valid E-mail!',
+      }, {
+        required: true, message: 'Please input your E-mail!',
+      }],
+    })(
+      <Input type="text" className="input-email" />
+    )}
+  </FormItem>
+       <FormItem>
+
+         <div><Button  id="login-form-button" htmlType="submit" className="login-form-button">
+           Reset Password
+         </Button></div>
+
+       </FormItem>
+     </Form>
+     </div>
+     </Col>
+   );
+ }
+}
+const WrappedForgetPassword = Form.create()(ForgetPassword);
+
+export default WrappedForgetPassword;
