dcd2957b7df35a9372e7f503461f7799a9fe6e45
sau9458 <saurabh_ks@excellencetechnologies.in>
2018-02-19 10:26:25 +0530

    screen7 added

--- src/component/HomePage.js
+++ src/component/HomePage.js
@@ -3,7 +3,7 @@ import { Col,Form, Icon, Input, Button,Tabs} from 'antd';
 import Calandar from './Calendar';
 import Inquiries from './Inquiries' ;
 import Inovice from './Inovice';
-
+import Profile from './Profile';
 const TabPane = Tabs.TabPane;
 class HomePage extends React.Component {
   render(){
@@ -20,7 +20,7 @@ class HomePage extends React.Component {
                   <span >Inovice</span></span>} key="3"><Inovice/></TabPane>
 
                   <TabPane  tab={<span><span id="tabs-icon"><Icon type="user" /><br/></span>
-                  <span >Profile</span></span>}key="4"></TabPane>
+                  <span >Profile</span></span>}key="4"><Profile/></TabPane>
           </Tabs>
     );
   }
